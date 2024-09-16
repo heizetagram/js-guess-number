8e4e596d4480a9fae961fb39e090527d2277e261
heizetagram <davidlu1999@gmail.com>
2024-09-16 21:59:51 +0200

    test

--- js/guessnumber.js
+++ js/guessnumber.js
@@ -1,48 +1,73 @@
 const labelMessage = document.querySelector(".message");
-
 const labelNumber = document.querySelector(".number");
-
 const actScore = document.querySelector(".score");
-
 const inputGuess = document.querySelector(".guess");
-
 const againButton = document.querySelector(".again");
 const checkButton = document.querySelector(".check");
+const bdy = document.querySelector("body");
 
 ///////////////////////////////////////////////////////////////////////
-let randomNumber = 0;
+let randomNumber;
 generateNumber();
 function generateNumber() {
     randomNumber = Math.trunc(Math.random() * 20);
-    labelMessage.textContent = 'Start guessing';
+    labelMessage.textContent = 'Start guessing...';
+    bdy.style.background = "#222";
 }
 
 againButton.addEventListener('click', generateNumber);
 
 ///////////////////////////////////////////////////////////////////////
-let score = document.querySelector(".score");
+let score = parseInt(actScore.textContent);
+
+function checkScoreZero() {
+    if (score === 0) {
+        youLose();
+    }
+}
+
 function plusScore() {
-    score.value = score++;
-    actScore.textContent = score.textContent;
+    score++;
+    actScore.textContent = score.toString();
+    checkScoreZero();
+}
+
+function minusScore() {
+    score--;
+    actScore.textContent = score.toString();
+    checkScoreZero();
 }
 
 ///////////////////////////////////////////////////////////////////////
 
 function checkResult() {
-    let inpGuess = Number(inputGuess.value);
-    if (inpGuess === randomNumber) {
-        labelMessage.textContent = 'Du har gættet tallet';
+    let inpGuess = parseInt(inputGuess.value);
+
+    if (score === 0) {
+        checkScoreZero();
+    } else if (inpGuess === randomNumber) {
+        labelMessage.textContent = 'Du har gættet tallet!';
         labelNumber.textContent = randomNumber;
         plusScore();
+        youWin();
     } else if (inpGuess > randomNumber) {
         labelMessage.textContent = 'For højt';
-        score.value = score--;
+        minusScore();
     } else if (inpGuess < randomNumber) {
         labelMessage.textContent = 'For lavt';
-        score.value = score--;
+        minusScore();
     }
 }
 
 checkButton.addEventListener('click', checkResult);
 
 ///////////////////////////////////////////////////////////////////////
+
+function youWin() {
+    bdy.style.background = "green";
+}
+
+function youLose() {
+    actScore.textContent = "0";
+    bdy.style.background = "red";
+}
