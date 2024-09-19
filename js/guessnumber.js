903a1dbb7bbcba6f4aa5dd8b5610405efae6d897
David Lu <davidlu1999@gmail.com>
2024-09-19 14:31:19 +0200

    Added high score, close guess, reset screen

--- js/guessnumber.js
+++ js/guessnumber.js
@@ -1,25 +1,38 @@
 const labelMessage = document.querySelector(".message");
 const labelNumber = document.querySelector(".number");
 const actScore = document.querySelector(".score");
+const highscore = document.querySelector(".highscore");
 const inputGuess = document.querySelector(".guess");
 const againButton = document.querySelector(".again");
 const checkButton = document.querySelector(".check");
 const bdy = document.querySelector("body");
 
 ///////////////////////////////////////////////////////////////////////
+
 let randomNumber;
+let hsNum = 0;
+
+// Start the round by generating a random number
 generateNumber();
+// Generate an integer between 1-20
 function generateNumber() {
-    randomNumber = Math.trunc(Math.random() * 20);
+    randomNumber = Math.trunc(Math.random() * 20 + 1);
+}
+
+// Reset game
+function newGame() {
+    generateNumber()
     labelMessage.textContent = 'Start guessing...';
     bdy.style.background = "#222";
 }
 
-againButton.addEventListener('click', generateNumber);
+againButton.addEventListener('click', newGame);
 
 ///////////////////////////////////////////////////////////////////////
+
 let score = parseInt(actScore.textContent);
 
+// Check if current score is 0
 function checkScoreZero() {
     if (score === 0) {
         youLose();
@@ -36,6 +49,14 @@ function minusScore() {
     score--;
     actScore.textContent = score.toString();
     checkScoreZero();
+    resetScreen()
+}
+
+function checkHighScore(score) {
+    if (score > hsNum) {
+        hsNum = score;
+        highscore.textContent = hsNum;
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////
@@ -49,7 +70,12 @@ function checkResult() {
         labelMessage.textContent = 'Du har gættet tallet!';
         labelNumber.textContent = randomNumber;
         plusScore();
+        checkHighScore(score)
         youWin();
+    } else if (inpGuess === (randomNumber - 1) || inpGuess === (randomNumber + 1)) {
+        labelMessage.textContent = 'Tæt på'
+        minusScore()
+        youreClose()
     } else if (inpGuess > randomNumber) {
         labelMessage.textContent = 'For højt';
         minusScore();
@@ -65,9 +91,21 @@ checkButton.addEventListener('click', checkResult);
 
 function youWin() {
     bdy.style.background = "green";
+    generateNumber()
+}
+
+function youreClose() {
+    bdy.style.background = "orange"
 }
 
 function youLose() {
     actScore.textContent = "0";
     bdy.style.background = "red";
+}
+
+///////////////////////////////////////////////////////////////////////
+
+function resetScreen() {
+    labelNumber.textContent = "?";
+    bdy.style.background = "#222";
 }
